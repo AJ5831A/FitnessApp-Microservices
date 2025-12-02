633a6f0d3a9b4ff6671331ad3fefe30ed1a99212
AJ5831A <jakhararyan27@gmail.com>
2025-12-03 00:08:27 +0530

    Ai response being sabed in MongoDB

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,9 +13,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class ActivityMessageListener {
+    private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
         log.info("Activity received for processing: {}" , activity.getId());
+        //log.info("Generated Response: {}" , activityAIService.generateRecommendation(activity));
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -1,5 +1,44 @@
 package com.fitness.aiservice.service;
 
-public class GeminiService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Map;
+
+@Service
+public class GeminiService {
+    private final WebClient webClient;
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getAnswer(String prompt){
+        Map<String , Object> reqBody = Map.of(
+                "contents" , new Object[]{
+                        Map.of(
+                                "parts" , new Object[]{
+                                        Map.of("text" , prompt)
+                                }
+                        )
+                }
+        );
+
+        return webClient.post()
+                .uri(geminiApiUrl)
+                .header("x-goog-api-key" , geminiApiKey)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(reqBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+    }
 }
